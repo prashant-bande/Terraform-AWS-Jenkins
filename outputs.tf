123a3c9da9649f289c952552f03bd8bdcc8532fd
prashant-bande <45324633+prashant-bande@users.noreply.github.com>
2020-12-19 00:39:47 +0530

    Terraform code

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "Jenkins-Main-Node-Public-IP" {
+  value = aws_instance.jenkins-master.public_ip
+}
+
+output "Jenkins-Worker-Public-IPs" {
+  value = {
+    for instance in aws_instance.jenkins-worker-oregon :
+    instance.id => instance.public_ip
+  }
+}
+
+output "LB_DNS_NAME" {
+  value = aws_lb.application-lb.dns_name
+}
